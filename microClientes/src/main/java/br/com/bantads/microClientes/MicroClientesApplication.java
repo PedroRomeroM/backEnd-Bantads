95ed9add83caa044fe1eb8e55f712afbbb0c2992
PedroRomeroM <122692587+PedroRomeroM@users.noreply.github.com>
2023-04-15 20:20:28 -0300

    criando o projeto

---
+++ microClientes/src/main/java/br/com/bantads/microClientes/MicroClientesApplication.java
@@ -0,0 +1,13 @@
+package br.com.bantads.microClientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroClientesApplication.class, args);
+	}
+
+}
---
+++ microClientes/src/test/java/br/com/bantads/microClientes/MicroClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bantads.microClientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
