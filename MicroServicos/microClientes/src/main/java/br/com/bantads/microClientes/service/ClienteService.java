ce327b65667cf3ed7abf6fff1b4912597726eb3b
PedroRomeroM <122692587+PedroRomeroM@users.noreply.github.com>
2023-04-16 15:42:35 -0300

    crud

--- MicroServicos/microClientes/src/main/java/br/com/bantads/microClientes/service/ClienteService.java
+++ MicroServicos/microClientes/src/main/java/br/com/bantads/microClientes/service/ClienteService.java
@@ -1,8 +1,13 @@
 package br.com.bantads.microClientes.service;
 
+import br.com.bantads.microClientes.dto.ClienteDto;
+import br.com.bantads.microClientes.model.Cliente;
 import br.com.bantads.microClientes.repository.ClienteRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,6 +19,34 @@ public class ClienteService {
     @Autowired
     private ModelMapper modelMapperCliente;
 
-    
+    public Page<ClienteDto> selectAllClientes(Pageable page) {
+        return clienteRepository
+                .findAll(page)
+                .map(e -> modelMapperCliente.map(e, ClienteDto.class));
+    }
+
+    public ClienteDto selectClienteById(Integer clientId) {
+        Cliente cliente = clienteRepository.findById(clientId)
+                .orElseThrow(() -> new EntityNotFoundException());
+
+        return modelMapperCliente.map(cliente, ClienteDto.class);
+    }
+
+    public ClienteDto createCliente(ClienteDto dto) {
+        Cliente cliente = modelMapperCliente.map(dto, Cliente.class);
+        clienteRepository.save(cliente);
+
+        return modelMapperCliente.map(cliente, ClienteDto.class);
+    }
+    public ClienteDto atualizarCliente(int clienteId, ClienteDto dto) {
+        Cliente cliente = modelMapperCliente.map(dto, Cliente.class);
+        cliente.setClientId(clienteId);
+        cliente = clienteRepository.save(cliente);
+        return modelMapperCliente.map(cliente, ClienteDto.class);
+    }
+
+    public void deleteCliente (int clienteId) {
+        clienteRepository.deleteById(clienteId);
+    }
 
 }
