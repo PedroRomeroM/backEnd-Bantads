c734dc7e2cf95901311c1ce8170a72a7a8741bcc
andradeuser <andradeclashofclans17@gmail.com>
2023-06-06 18:47:51 -0300

    microConta

---
+++ MicroServicos/microConta/src/main/java/bantadsBack/microConta/MicroContaApplication.java
@@ -0,0 +1,13 @@
+package bantadsBack.microConta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroContaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroContaApplication.class, args);
+	}
+
+}
