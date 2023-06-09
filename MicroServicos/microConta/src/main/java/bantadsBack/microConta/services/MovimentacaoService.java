108976bbc5cd7ae35e060b8f3206834f2430076c
andradeuser <andradeclashofclans17@gmail.com>
2023-06-09 16:19:08 -0300

    modulo-conta atualizado 2

--- MicroServicos/microConta/src/main/java/bantadsBack/microConta/services/MovimentacaoService.java
+++ MicroServicos/microConta/src/main/java/bantadsBack/microConta/services/MovimentacaoService.java
@@ -17,109 +17,109 @@ import java.util.Optional;
 @Service
 public class MovimentacaoService {
 
-//    @Autowired
-//    private ContaRepositoryCUD repositoryCUD;
-//    @Autowired
-//    private MovimentacaoRepositoryCUD movimentacaoRepositoryCUD;
-//    @Autowired
-//    private MovimentacaoRepositoryR movimentacaoRepositoryR;
-//
-//    @Autowired
-//    private ModelMapper mapper;
-//
-//
-//    public MovimentacoesDTO despositar(String destinoMovimentacao, Float valorMovimentacao){
-//        MovimentacoesCUD op = new MovimentacoesCUD();
-//
-//        Optional<ContaCUD> conta = repositoryCUD.findByIdCliente(destinoMovimentacao);
-//
-//        op.setDestinoMovimentacao(String.valueOf(conta.get()));
-//        op.setOrigemMovimentacao(null);
-//        op.setValorMovimentacao(valorMovimentacao);
-//        op.setTipoMovimentacao(1);
-//        op.setDataMovimentacao(null);
-//
-//        op = movimentacaoRepositoryCUD.save(op);
-//
-//        MovimentacoesDTO DTO = mapper.map(op, MovimentacoesDTO.class);
-//
-//        BigDecimal saldo = conta.get().getSaldoConta().add(BigDecimal.valueOf(valorMovimentacao));
-//
-//        updateSaldo(destinoMovimentacao, saldo);
-//
-//        return DTO;
-//    }
-//
-//    public MovimentacoesDTO sacar(String destinoMovimentacao, Float valorMovimentacao) {
-//
-//        MovimentacoesCUD op = new MovimentacoesCUD();
-//        Optional<ContaCUD> conta = repositoryCUD.findByIdCliente(destinoMovimentacao);
-//
-//        BigDecimal valorPossivel = conta.get().getSaldoConta().add(BigDecimal.valueOf(conta.get().getLimiteConta()));
-//
-//        if (valorPossivel.compareTo(BigDecimal.valueOf(valorMovimentacao)) < 0);
-//
-//        op.setDestinoMovimentacao(null);
-//        op.setOrigemMovimentacao(String.valueOf(conta.get()));
-//        op.setValorMovimentacao(valorMovimentacao);
-//        op.setTipoMovimentacao(2);
-//        op.setDataMovimentacao(null);
-//
-//        op = movimentacaoRepositoryCUD.save(op);
-//
-//        MovimentacoesDTO dto = mapper.map(op, MovimentacoesDTO.class);
-//
-//        BigDecimal saldo = conta.get().getSaldoConta().subtract(BigDecimal.valueOf(valorMovimentacao));
-//
-//        updateSaldo(destinoMovimentacao, saldo);
-//
-//        return dto;
-//    }
-//
-//
-//    public MovimentacoesDTO transferir(String origemMovimentacao, String destinoMovimentacao, Float valorMovimentacao) {
-//
-//        MovimentacoesCUD op = new MovimentacoesCUD();
-//
-//        Optional<ContaCUD> deConta = repositoryCUD.findByIdCliente(origemMovimentacao);
-//        Optional<ContaCUD> paraConta = repositoryCUD.findByIdCliente(destinoMovimentacao);
-//
-//        BigDecimal valorPossivel = deConta.get().getSaldoConta().add(BigDecimal.valueOf(deConta.get().getLimiteConta()));
-//
-//        if (valorPossivel.compareTo(BigDecimal.valueOf(valorMovimentacao)) < 0);
-//
-//        op.setIdMovimentacao(null);
-//        op.setDestinoMovimentacao(String.valueOf(paraConta.get()));
-//        op.setOrigemMovimentacao(String.valueOf(deConta.get()));
-//        op.setValorMovimentacao(valorMovimentacao);
-//        op.setTipoMovimentacao(3);
-//        op.setDataMovimentacao(null);
-//
-//        op = movimentacaoRepositoryCUD.save(op);
-//        MovimentacoesDTO dto = mapper.map(op, MovimentacoesDTO.class);
-//
-//        BigDecimal saldoDe = deConta.get().getSaldoConta().subtract(BigDecimal.valueOf(valorMovimentacao));
-//        updateSaldo(origemMovimentacao, saldoDe);
-//
-//
-//        BigDecimal saldoPara = paraConta.get().getSaldoConta().add(BigDecimal.valueOf(valorMovimentacao));
-//        updateSaldo(destinoMovimentacao, saldoPara);
-//
-//        return dto;
-//    }
-//
-//
-//    public ContaCUD updateSaldo(String numConta, BigDecimal saldoConta){
-//
-//        Optional<ContaCUD> conta = repositoryCUD.findByIdCliente(numConta);
-//        if (conta.isPresent());
-//            ContaCUD ct = conta.get();
-//            ct.setSaldoConta(saldoConta);
-//            ct = repositoryCUD.save(ct);
-//            ContaDTO dto = mapper.map(ct, ContaDTO.class);
-//
-//            return ct;
-//    }
+    @Autowired
+    private ContaRepositoryCUD repositoryCUD;
+    @Autowired
+    private MovimentacaoRepositoryCUD movimentacaoRepositoryCUD;
+    @Autowired
+    private MovimentacaoRepositoryR movimentacaoRepositoryR;
+
+    @Autowired
+    private ModelMapper mapper;
+
+
+    public MovimentacoesDTO despositar(Long destinoMovimentacao, BigDecimal valorMovimentacao){
+        MovimentacoesCUD op = new MovimentacoesCUD();
+
+        Optional<ContaCUD> conta = repositoryCUD.findByIdCliente(destinoMovimentacao);
+
+        op.setParaCliente(conta.get());
+        op.setDeCliente(null);
+        op.setValorMovimentacao(valorMovimentacao);
+        op.setTipoMovimentacao("D");
+        op.setDataTempoMovimentacao(null);
+
+        op = movimentacaoRepositoryCUD.save(op);
+
+        MovimentacoesDTO DTO = mapper.map(op, MovimentacoesDTO.class);
+
+        BigDecimal saldo = conta.get().getSaldoConta().add(valorMovimentacao);
+
+        updateSaldo(destinoMovimentacao, saldo);
+
+        return DTO;
+    }
+
+    public MovimentacoesDTO sacar(Long destinoMovimentacao, Float valorMovimentacao) {
+
+        MovimentacoesCUD op = new MovimentacoesCUD();
+        Optional<ContaCUD> conta = repositoryCUD.findByIdCliente(destinoMovimentacao);
+
+        BigDecimal valorPossivel = conta.get().getSaldoConta().add(conta.get().getLimiteConta());
+
+        if (valorPossivel.compareTo(BigDecimal.valueOf(valorMovimentacao)) < 0);
+
+        op.setParaCliente(null);
+        op.setDeCliente(conta.get());
+        op.setValorMovimentacao(BigDecimal.valueOf(valorMovimentacao));
+        op.setTipoMovimentacao("S");
+        op.setDataTempoMovimentacao(null);
+
+        op = movimentacaoRepositoryCUD.save(op);
+
+        MovimentacoesDTO dto = mapper.map(op, MovimentacoesDTO.class);
+
+        BigDecimal saldo = conta.get().getSaldoConta().subtract(BigDecimal.valueOf(valorMovimentacao));
+
+        updateSaldo(destinoMovimentacao, saldo);
+
+        return dto;
+    }
+
+
+    public MovimentacoesDTO transferir(Long origemMovimentacao, Long destinoMovimentacao, Float valorMovimentacao) {
+
+        MovimentacoesCUD op = new MovimentacoesCUD();
+
+        Optional<ContaCUD> deConta = repositoryCUD.findByIdCliente(origemMovimentacao);
+        Optional<ContaCUD> paraConta = repositoryCUD.findByIdCliente(destinoMovimentacao);
+
+        BigDecimal valorPossivel = deConta.get().getSaldoConta().add(deConta.get().getLimiteConta());
+
+        if (valorPossivel.compareTo(BigDecimal.valueOf(valorMovimentacao)) < 0);
+
+        op.setIdMovimentacao(null);
+        op.setParaCliente(paraConta.get());
+        op.setDeCliente(deConta.get());
+        op.setValorMovimentacao(BigDecimal.valueOf(valorMovimentacao));
+        op.setTipoMovimentacao("T");
+        op.setDataTempoMovimentacao(null);
+
+        op = movimentacaoRepositoryCUD.save(op);
+        MovimentacoesDTO dto = mapper.map(op, MovimentacoesDTO.class);
+
+        BigDecimal saldoDe = deConta.get().getSaldoConta().subtract(BigDecimal.valueOf(valorMovimentacao));
+        updateSaldo(origemMovimentacao, saldoDe);
+
+
+        BigDecimal saldoPara = paraConta.get().getSaldoConta().add(BigDecimal.valueOf(valorMovimentacao));
+        updateSaldo(destinoMovimentacao, saldoPara);
+
+        return dto;
+    }
+
+
+    public ContaCUD updateSaldo(Long idConta, BigDecimal saldoConta){
+
+        Optional<ContaCUD> conta = repositoryCUD.findByIdCliente(idConta);
+        if (conta.isPresent());
+            ContaCUD ct = conta.get();
+            ct.setSaldoConta(saldoConta);
+            ct = repositoryCUD.save(ct);
+            ContaDTO dto = mapper.map(ct, ContaDTO.class);
+
+            return ct;
+    }
 
 
 }
