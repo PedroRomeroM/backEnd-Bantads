42468a56e3553d5275ac6edd662a0c6436de7948
andradeuser <andradeclashofclans17@gmail.com>
2023-06-17 20:07:31 -0300

    deus

--- MicroServicos/microConta/src/main/java/bantadsBack/microConta/controllers/ContaController.java
+++ MicroServicos/microConta/src/main/java/bantadsBack/microConta/controllers/ContaController.java
@@ -1,17 +1,21 @@
 package bantadsBack.microConta.controllers;
 
 
+import bantadsBack.microConta.dtos.ConsultaGerenteDTO;
 import bantadsBack.microConta.dtos.sagaCadastrarCliente.ContaDTO;
 import bantadsBack.microConta.repositoryCUD.ContaRepositoryCUD;
 import bantadsBack.microConta.repositoryR.ContaRepositoryR;
 import bantadsBack.microConta.services.ContaService;
 import bantadsBack.microConta.services.MovimentacaoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/conta")
 public class ContaController {
 
@@ -27,32 +31,26 @@ public class ContaController {
     @Autowired
     private ContaRepositoryCUD contaRepositoryCUD;
 
-//    @PostMapping
-//    public ResponseEntity<ContaDTO> criarConta(@NotNull @RequestBody ClienteDTO clienteDTO){
-//        ContaDTO criar = contaService.criarConta(clienteDTO.getIdCliente());
-//        return ResponseEntity.status(HttpStatus.OK).body(criar);
-//    }
 
-    @PutMapping(value = "/conta/{id}")
-    public ResponseEntity<ContaDTO> updateConta(@PathVariable("id") Long id, @RequestBody ContaDTO account) throws Exception {
-        try{
-            ContaDTO contaAtualizada = contaService.updateConta(id, account);
-            return ResponseEntity.ok().body(contaAtualizada);
-        }catch (Exception e) {
-            throw new Exception(e);
-        }
-    }
-
-
-//    @GetMapping(value = "/testeauth")
-//    public ResponseEntity<Object> teste() throws Exception {
-//        try {
-//            Long id = contaService.tranferirCliente();
-//            return new ResponseEntity<>(id, HttpStatus.CREATED);
+//    @PutMapping(value = "/conta/{id}")
+//    public ResponseEntity<ContaDTO> updateConta(@PathVariable("id") Long id, @RequestBody ContaDTO account) throws Exception {
+//        try{
+//            ContaDTO contaAtualizada = contaService.updateConta(id, account);
+//            return ResponseEntity.ok().body(contaAtualizada);
 //        }catch (Exception e) {
 //            throw new Exception(e);
 //        }
 //    }
+
+    @GetMapping("/admin")
+    public ResponseEntity<List<ConsultaGerenteDTO>> telaInicialAdmin(){
+
+        List<ConsultaGerenteDTO> i = contaService.consultarGerenteTelaInicialAdmin();
+        return ResponseEntity.status(HttpStatus.OK).body(i);
+    }
+
+
+
 
 
 }
--- MicroServicos/orquestrador/src/main/java/br/com/bantads/orquestrador/controller/Controller.java
+++ MicroServicos/orquestrador/src/main/java/br/com/bantads/orquestrador/controller/Controller.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:4200")
 public class Controller {
     @Autowired
     private RabbitTemplate rabbitTemplate;
