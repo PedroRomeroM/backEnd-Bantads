822e776a9a99354264369628e39c6c93012e1be9
andradeuser <andradeclashofclans17@gmail.com>
2023-06-20 00:08:51 -0300

    update gerente

--- MicroServicos/microGerentes/src/main/java/bantadsbackend/microGerentes/repository/GerenteRepository.java
+++ MicroServicos/microGerentes/src/main/java/bantadsbackend/microGerentes/repository/GerenteRepository.java
@@ -3,8 +3,10 @@ package bantadsbackend.microGerentes.repository;
 import bantadsbackend.microGerentes.model.Gerente;
 import bantadsbackend.microGerentes.model.GerenteEditar;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface GerenteRepository extends JpaRepository<Gerente, Integer> {
 
@@ -15,6 +17,11 @@ public interface GerenteRepository extends JpaRepository<Gerente, Integer> {
     @Query(value = "SELECT gerente_id FROM gerentes WHERE cpf_gerente = :cpf", nativeQuery = true)
     Integer getGerenteId(@Param("cpf") String cpf);
 
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Gerente SET nomeGerente = :novoNome, emailGerente = :novoEmail, telefoneGerente = :novoTelefone WHERE gerenteId = :gerenteId")
+    int updateGerente(@Param("novoNome") String novoNome, @Param("novoEmail") String novoEmail, @Param("novoTelefone") String novoTelefone, @Param("gerenteId") Long gerenteId);
 
 
 }
